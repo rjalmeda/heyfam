2d163a8bb8d6d2fcc0f1272a76d6d3ae465922dd
RJ Almeda <rjalmeda@gmail.com>
2021-08-20 09:07:53 -0700

    change port

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
-const port = 4040;
+const port = process.env.PORT || 4040;
 
 const connections = [];
 const clients = [];
